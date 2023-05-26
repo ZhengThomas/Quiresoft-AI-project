2c89d710cc56c4b3171aeaa062a01d6fc484f402
ZhengThomas <mrthomaszheng@gmail.com>
2023-05-26 14:27:34 -0700

    creation of the login page, sorta unpolished

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
+import {LoginPage} from './Components/LoginPage.js'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
-
+  <LoginPage />
 );
